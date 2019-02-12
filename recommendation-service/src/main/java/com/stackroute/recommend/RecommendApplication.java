ab98a75de469f56385713c969731c3b9d3ec2c15
boeingwave3-kishlay <verma.kishlay688@gmail.com>
2019-02-12 18:16:03 +0530

    Added the API gateway links and Eureka Clients

--- recommendation-service/src/main/java/com/stackroute/recommend/RecommendApplication.java
+++ recommendation-service/src/main/java/com/stackroute/recommend/RecommendApplication.java
@@ -2,8 +2,9 @@ package com.stackroute.recommend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Component;
 
-
+@Component
 @EnableEurekaClient
 @SpringBootApplication
 public class RecommendApplication {
