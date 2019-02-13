9c6357b61c378850994b5c75932779dd91dc8698
boeingwave3-kishlay <verma.kishlay688@gmail.com>
2019-02-14 00:09:37 +0530

    Reconfigured Eureka Client config for Userprofile services

--- userprofile-service/src/main/java/com/stackroute/service/KafkaListenerService.java
+++ userprofile-service/src/main/java/com/stackroute/service/KafkaListenerService.java
@@ -3,7 +3,6 @@ package com.stackroute.service;
 import com.stackroute.repository.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
