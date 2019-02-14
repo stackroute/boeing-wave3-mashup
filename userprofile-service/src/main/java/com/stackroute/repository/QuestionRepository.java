d03eda63077b752de4ccb84f14985e657073698d
iamkishlayverma <verma.kishlay688@gmail.com>
2019-02-14 09:19:48 +0000

    User profile removed

--- userprofile-service/src/main/java/com/stackroute/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackroute.repository;
-
-import com.stackroute.domain.Question;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuestionRepository extends MongoRepository<Question, Integer> {
-}
