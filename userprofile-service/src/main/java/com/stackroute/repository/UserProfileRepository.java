ed4d1e7ead2abbd2656117ad0d1c2a0c0700cb3b
Ujjawala <33411716+Ujjawala@users.noreply.github.com>
2019-02-21 12:10:41 +0530

    Update UserProfileRepository.java

--- userprofile-service/src/main/java/com/stackroute/repository/UserProfileRepository.java
+++ userprofile-service/src/main/java/com/stackroute/repository/UserProfileRepository.java
@@ -3,10 +3,6 @@ package com.stackroute.repository;
 import com.stackroute.domain.UserProfile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
+// repository for userprofile
 @Repository
-public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
-//    @Query(value = "SELECT * FROM track WHERE name=?1", count = true)
-//    public List<UserProfile> findByName(String name);
-
-}
+public interface UserProfileRepository extends MongoRepository<UserProfile, String> {}
