418a39efa6078e3223041606d77bf072ed6668f8
Ujjawala <33411716+Ujjawala@users.noreply.github.com>
2019-02-27 11:53:34 +0530

    Update UserprofileServiceApplication.java

--- userprofile-service/src/main/java/com/stackroute/UserprofileServiceApplication.java
+++ userprofile-service/src/main/java/com/stackroute/UserprofileServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// main class for running the application
 @EnableEurekaClient
 @SpringBootApplication
 public class UserprofileServiceApplication {
