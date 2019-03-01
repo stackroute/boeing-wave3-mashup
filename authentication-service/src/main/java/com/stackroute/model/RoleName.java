10a4b2961a55cbda1e1e6b1fd69646696f9b6264
siddharth106 <siddharthagarwal177@h=gmail.com>
2019-03-01 09:43:42 +0530

    deleted privious version

--- authentication-service/src/main/java/com/stackroute/model/RoleName.java
+++
@@ -1,5 +0,0 @@
-package com.stackroute.model;
-
-public enum  RoleName {
-    ROLE_USER,
-}
--- submission-service/src/main/java/com/stackroute/service/SubmissionService.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.service;
-
-import com.stackroute.domain.SubmissionData;
-
-public interface SubmissionService {
-    public SubmissionData saveSubmission(SubmissionData submissionData);
-
-    public SubmissionData getSubmission(String username, int questionId);
-}
