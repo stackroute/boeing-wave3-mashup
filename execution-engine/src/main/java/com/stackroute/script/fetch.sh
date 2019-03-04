9136c00c4298f9efdb15c72508ae9569457a154e
siddharth106 <siddharthagarwal177@h=gmail.com>
2019-03-04 19:21:19 +0530

    new execution engine added

--- execution-engine/src/main/java/com/stackroute/script/fetch.sh
+++ execution-engine/src/main/java/com/stackroute/script/fetch.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-mkdir namita
-cd namita
-repositoryBP="https://github.com/aroranamita09/mashupBoilerplate.git"
-git clone "$repositoryBP"
-cd mashupBoilerplate
-cd src
-cd main
+echo $1
 repository=$1
-echo "......................................."
+username=$2
+mkdir ./src/$2
+cd  ./src/$2
+cd ../
+
+echo "printging in the fetch.sh file ...."
 echo $repository
-git clone "$repository"
+#localFolder="/home/user/Music/boeing-wave3-mashup/execution-engine/src/newlyCreated"
+git clone "$repository"  $2
+
+echo "running fetch.sh finished successfully"
--- execution-engine/src/main/java/com/stackroute/script/run.sh
+++ execution-engine/src/main/java/com/stackroute/script/run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-touch sdf
-cd /home/user/IdeaProjects/executionengine
+echo "inside the run.sh script "
+echo $1
+pwd
+cd ./src/$1
+echo "printing the current directory"
+pwd
+echo "running maven package life cycle"
 alias mvn='mvn  --quiet'
-mvn test -l compile.log
+mvn test -l   compile.log
+sed '2,5d' compile.log > compile2.log
