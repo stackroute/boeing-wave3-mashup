61c075d8070fd608873dc12804cbd24e8a1ab5ec
keshav174 <keshavkumar174@gmail.com>
2019-03-05 15:24:02 +0530

    major changes to execution engine

--- execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
+++ execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
@@ -2,12 +2,11 @@ package com.stackroute.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@CrossOrigin("*")
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
--- execution-engine/src/main/java/com/stackroute/service/FileWriterService.java
+++
@@ -1,7 +0,0 @@
-//package com.stackroute.service;
-//
-//import java.io.IOException;
-//
-//public interface FileWriterService {
-//    public void randomAcessFile() throws IOException;
-//}
