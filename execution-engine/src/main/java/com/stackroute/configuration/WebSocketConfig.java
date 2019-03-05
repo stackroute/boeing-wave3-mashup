1eb293784af89fca2edede3bcbc9c83f3211733a
ujjawala.yati <ujjawala.yati@boeing.com>
2019-03-05 20:30:06 +0530

    changes to cross policy

--- execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
+++ execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/gkz");
+        config.setApplicationDestinationPrefixes("/gkz-stomp-endpoint");
     }
 
     public void registerStompEndpoints(StompEndpointRegistry registry) {
