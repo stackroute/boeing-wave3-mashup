7dd6aabeec1aa00ac2f08153efe4bb4ea9d7088f
ujjawala.yati <ujjawala.yati@boeing.com>
2019-03-05 12:25:47 +0530

    cross origin added in js

--- js-execution-engine/src/main/java/com/stackroute/controller/WebController.java
+++ js-execution-engine/src/main/java/com/stackroute/controller/WebController.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-
+@CrossOrigin(value = "*")
 @Controller
 public class WebController {
 
