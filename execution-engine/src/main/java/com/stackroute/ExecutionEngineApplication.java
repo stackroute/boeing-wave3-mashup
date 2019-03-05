14ecf2520ea78311b9d92f208bfe619be1533c77
boeingwave3-kishlay <verma.kishlay688@gmail.com>
2019-03-05 10:03:00 +0530

    Fixed the CORS Policy error

--- execution-engine/src/main/java/com/stackroute/ExecutionEngineApplication.java
+++ execution-engine/src/main/java/com/stackroute/ExecutionEngineApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -19,5 +23,18 @@ public class ExecutionEngineApplication {
 		return builder.build();
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod(HttpMethod.GET.name());
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+	}
 }
 
