f43b417bace50a423628fec745cc58cc89778756
ujjawala.yati <ujjawala.yati@boeing.com>
2019-03-05 12:07:41 +0530

    eureka server added in js

--- js-execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
+++ js-execution-engine/src/main/java/com/stackroute/configuration/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.stackroute.configuration;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@EnableEurekaClient
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
