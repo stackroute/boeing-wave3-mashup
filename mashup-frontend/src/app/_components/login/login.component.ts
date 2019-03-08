8d899fbdb01ece84b5614cb7945e89155886c7a3
ishumatil <ishumatil000@gmail.com>
2019-03-08 09:55:43 +0530

    some changes done in frontend

--- mashup-frontend/src/app/_components/login/login.component.ts
+++ mashup-frontend/src/app/_components/login/login.component.ts
@@ -42,7 +42,7 @@ export class LoginComponent implements OnInit {
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.tokenStorage.getAuthorities();
-        this.reloadPage();
+        // this.reloadPage();
         // this.router.navigate(['']);
       },
       error => {
