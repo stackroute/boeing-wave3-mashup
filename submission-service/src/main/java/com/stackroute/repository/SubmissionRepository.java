b362756ef80a530ad00f63f02ec8507a2915ed87
boeingwave3-kishlay <verma.kishlay688@gmail.com>
2019-03-12 10:13:48 +0530

    Changes in submission service for id error

--- submission-service/src/main/java/com/stackroute/repository/SubmissionRepository.java
+++ submission-service/src/main/java/com/stackroute/repository/SubmissionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SubmissionRepository extends MongoRepository<SubmissionData, String> {
 
+    // Method to get submission data from database using username and questionID
     SubmissionData getSubmissionDataByUsernameAndQuestionId(String username, int questionId);
 }
