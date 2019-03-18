b9f5c4ac6848e21859c883d62d18f86676dd2f1d
ujjawala.yati <ujjawala.yati@boeing.com>
2019-03-18 16:20:04 +0530

    https in api n frontend

--- zuul-gatewayservice/src/main/java/com/stackroute/ZuulGatewayserviceApplication.java
+++ zuul-gatewayservice/src/main/java/com/stackroute/ZuulGatewayserviceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @RestController
 @RequestMapping("/api/v1/")
 @EnableZuulProxy
